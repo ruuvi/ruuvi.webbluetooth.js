06f7afac5aad817b0fde73bd60442073114ced97
Otso Jousimaa <otso@ojousima.net>
2017-03-10 16:20:32 +0200

    Call super before running child constructor

--- services/nordic_uart.js
+++ services/nordic_uart.js
@@ -18,6 +18,7 @@ require("../services/service_interface.js");
 class nordicUART extends serviceInterface {
 
   constructor(){
+  	super();
   	this.serviceName = "Nordic UART";
   	this.serviceUUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
   	this.characteristicUUIDs = { "TX": "6E400002-B5A3-F393-E0A9-E50E24DCCA9E",
