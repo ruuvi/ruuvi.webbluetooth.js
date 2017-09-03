9cdd2be36b832619be17804d72b9c30e1ab9deee
Otso Jousimaa <otso@ojousima.net>
2017-09-03 21:24:50 +0300

    Add stubs for functions

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ let connect = async function(deviceNamePrefix){
   }
   return handle;
 }
+
+let disconnect = async function(serverHandle){
+  try {
+    serverHandle.disconnect();
+  } catch(error) {
+    console.log("Error: " + error);
+  }
+}
+
+let getServices = async function(serverHandle){
+  let services = {};
+  try{
+    //TODO
+  } catch(error) {
+    console.log("Error: " + error);
+  }
+}
 /**
  *  connect(deviceNamePrefix) Starts asynchronous search for devices which advertise name that starts with prefix.
  *                            Example: connect("Ruuvi") to connect to devices with name "RuuviTag".
