2042980020261874887909f8580c94407aec682c
Otso Jousimaa <otso@ojousima.net>
2017-09-03 17:15:31 +0300

    Start conversion to nodejs

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let connect = async function(deviceNamePrefix){
+  let handle = {};
+  try {
+      let filters = [];
+      filters.push({namePrefix: deviceNamePrefix});
+      let options = {};
+      options.filters = filters;
+      //console.log(JSON.stringify(options));
+      //Connect to device with Ruuvi IMU service
+      let device = await navigator.bluetooth.requestDevice(options);
+      handle = await this.device.gatt.connect();
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  }
+  return handle;
+}
+/**
+ *  connect(deviceNamePrefix) Starts asynchronous search for devices which advertise name that starts with prefix.
+ *                            Example: connect("Ruuvi") to connect to devices with name "RuuviTag".
+ *                            Returns handle of the connection
+ *  disconnect(connection)    Disconnects given connection.
+ *  getServices(connection)   Returns array of service objects of given connection.
+ **/
+module.exports = {
+  connect           : connect,
+  disconnect        : disconnect,
+  getServices       : getServices
+}
