3b50f6f0a4cc2c826a861a71fd66444e3f0eedb9
Otso Jousimaa <otso@ojousima.net>
2017-09-04 20:28:28 +0300

    Add service connection

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-"use strict"
+/*jshint 
+    node: true
+ */
+"use strict";
+var NORDIC_UART = require("./services/nordic_uart.js");
+
+var handle = {};
+/** Known services **/
+var serviceList = [NORDIC_UART];
+/** Initialized services **/
+var servicesAvailable = {};
+
+let initServices = async function(serverHandle, services){
+  try{
+    console.log('Initializing services');
+      //Search through list of known services
+      for (const service of services) {
+        //If connection includes a known service
+        let uuid = service.uuid;
+        for(const interface of serviceList) {
+          if(uuid == interface.getServiceUUID()){
+            //Initialise service
+            await interface.init(handle);
+            //Add to list of services
+            let name = interface.getName();
+            servicesAvailable[name] = service;
+          }
+        }
+      }
+  } catch(error) {
+    console.log("Error: " + error);
+  }
+};
 
 let connect = async function(deviceNamePrefix){
-  let handle = {};
+
   try {
       let filters = [];
       filters.push({namePrefix: deviceNamePrefix});
@@ -9,11 +41,14 @@ let connect = async function(deviceNamePrefix){
       options.filters = filters;
       let device = await navigator.bluetooth.requestDevice(options);
       handle = await this.device.gatt.connect();
+      console.log('Getting Services...');
+      services = await server.getPrimaryServices();
+      await initServices(handle, services);
     } catch (error) {
       console.log("Error: " + error);
     }
   return handle;
-}
+};
 
 let disconnect = async function(serverHandle){
   try {
@@ -21,16 +56,11 @@ let disconnect = async function(serverHandle){
   } catch(error) {
     console.log("Error: " + error);
   }
-}
+};
 
-let getServices = async function(serverHandle){
-  let services = {};
-  try{
-    //TODO
-  } catch(error) {
-    console.log("Error: " + error);
-  }
-}
+let getServices = function(){
+  return servicesAvailable;
+};
 
 /**
  *  connect(deviceNamePrefix) Starts asynchronous search for devices which advertise name that starts with prefix.
@@ -43,4 +73,4 @@ module.exports = {
   connect           : connect,
   disconnect        : disconnect,
   getServices       : getServices
-}
+};
